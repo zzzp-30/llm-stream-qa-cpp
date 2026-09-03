ec721f275290aa0fea298d2bbba664c758bbc2c8
zzzp-30 <1770459850@qq.com>
2026-09-03 20:14:29 +0800

    Fix patch_gui.ps1: PE subsystem 2=GUI (was wrongly writing 3=Console)

--- qa_app/patch_gui.ps1
+++ qa_app/patch_gui.ps1
@@ -2,10 +2,13 @@ param([string]$exePath)
 $bytes = [System.IO.File]::ReadAllBytes($exePath)
 $peOffset = [BitConverter]::ToInt32($bytes, 60)
 $subsystemOffset = $peOffset + 92
-if ([BitConverter]::ToInt16($bytes, $subsystemOffset) -ne 3) {
-    $v = [BitConverter]::GetBytes([int16]3)
+# PE Subsystem: 2 = Windows GUI (无控制台), 3 = Windows Console
+if ([BitConverter]::ToInt16($bytes, $subsystemOffset) -ne 2) {
+    $v = [BitConverter]::GetBytes([int16]2)
     $bytes[$subsystemOffset] = $v[0]
     $bytes[$subsystemOffset + 1] = $v[1]
     [System.IO.File]::WriteAllBytes($exePath, $bytes)
-    Write-Host "Patched PE subsystem to Windows GUI"
+    Write-Host "Patched PE subsystem to Windows GUI (2)"
+} else {
+    Write-Host "Already Windows GUI subsystem (2)"
 }
